f2b36e70e7af60c28642de74f5fa4bd8e1019321
Rahul Powar <rahul@redsift.io>
2017-02-14 19:07:54 +0000

    Added value creation

--- src/Grid.js
+++ src/Grid.js
@@ -2,7 +2,8 @@
 
 const PNG = require('pngjs').PNG,
     fs = require('fs'),
-    Hexasphere = require('hexasphere.js');
+    Hexasphere = require('hexasphere.js'),
+    url = require('url');
 
 function latLonToXY(width, height, lat, lon) {
 
@@ -61,8 +62,23 @@ function computeCoverForTiles_box(png, tiles, radius) {
         if (xy.y > maxy) maxy = xy.y;
     }
 
-    if (maxx > png.width) maxx = png.width;
-    if (maxy > png.height) maxy = png.height;    
+    if ((maxx - minx) > png.width / 2) {
+        // wrap around
+        // hack, just take the positive block. causes mild distortion for earth
+        // could be done properly
+        maxx = 0;
+
+        for (let j = 0; j < tiles.boundary.length; j++) {
+            let latLon = tiles.getLatLon(radius, j);
+            let xy = latLonToXY(png.width, png.height, latLon.lat, latLon.lon);
+
+            if (xy.x > png.width / 2) {
+                continue;
+            }
+
+            if (xy.x > maxx) maxx = xy.x;
+        }
+    } 
 
     const w = (maxx - minx);
     const h = (maxy - miny);
@@ -72,15 +88,77 @@ function computeCoverForTiles_box(png, tiles, radius) {
 
     let count = 0;
     for (let i = 0; i < dst.data.length; i++) {
+        if ((i+1) % 4 === 0) {
+            // skip alpha channel
+            continue;
+        }
         count += 255 - dst.data[i];
     }
-    if (minx < 5) {
-        console.log(minx, miny, count, dst.data.length);   
+    
+    const score = count / ((dst.data.length / (4/3))  * 255);
 
-        dst.pack().pipe(fs.createWriteStream(`${minx}_${miny}.png`));
+    return score;
+}
+
+function mapColor(c, scale) {
+
+    for (let e of scale) {
+        if (e.color[0] === c[0] && e.color[1] === c[1] && e.color[2] === c[2]) {
+            return e.value;
+        }
     }
 
-    return count / (dst.data.length * 255);
+    console.log('Unknown color', c);
+    return 0;
+}
+
+function createValueFunction(value) {
+    if (value == null) return Promise.resolve(null);
+
+    let dfile = '_debug.png'; // TEMP
+
+    return new Promise((ok, ko) => {
+        const protocol = url.parse(value.url).protocol === 'https:' ? require('https') : require('http');
+        protocol.get(value.url, function(response) {
+            response.pipe(new PNG({
+                    filterType: 4
+            }))
+            .on('parsed', function() {
+                this.adjustGamma();
+
+                const w = value.crop.x2 - value.crop.x1;
+                const h = value.crop.y2 - value.crop.y1;
+
+                const remapped = new PNG({ width: w, height: h, colorType: 2 });
+                this.bitblt(remapped, value.crop.x1, value.crop.y1, w, h, 0, 0);
+
+                for (let i = 0; i < remapped.data.length; i += 4) {
+                    const v = mapColor([ remapped.data[i], remapped.data[i+1], remapped.data[i+2] ], value.scale) * 255;
+                    remapped.data[i] = v;
+                    remapped.data[i+1] = v;
+                    remapped.data[i+2] = v;
+                    remapped.data[i+3] = 255;
+                }
+
+                if (dfile) {
+                    remapped.pack()
+                        .pipe(fs.createWriteStream(dfile))
+                        .on('finish', function() {
+                            console.log(`debug PNG file ${dfile} written`);
+                        });
+                }
+
+                const fn = (tiles, radius) => {
+                    return computeCoverForTiles_box(remapped, tiles, radius);
+                };
+
+                ok(fn);        
+            })
+            .on('error', e => ko(`Could not parse PNG ${value.url}. ${e}`));
+        });
+
+        
+    });
 }
 
 function createGrid(map, p) {
@@ -91,67 +169,67 @@ function createGrid(map, p) {
     let tiny = p.tiny != null ? p.tiny : false;
     let threshold = p.threshold || 0.1;
     let box = p.box != null ? p.box : true;
-    let dfile = '_debug.png'; // TEMP
-
+    let ocean = p.ocean != null ? p.ocean : false; //TODO: Expose these options
+    let value = createValueFunction(p.value);
 
     const hexasphere = new Hexasphere(radius, divisions, width);
 
     const o = { src: map, tiles: [] };
 
-    return new Promise((ok) => {
-        if (map == null) {
-            throw new Error('No map supplied');
-        }
+    return value.then((valueFn) => {
+        return new Promise((ok) => {
+            if (map == null) {
+                throw new Error('No map supplied');
+            }
 
-        fs.createReadStream(map)
-        .pipe(new PNG({
-            filterType: 4
-        }))
-        .on('parsed', function() {
-            const debugBuffer = Buffer.alloc(this.data.length, 255);
+            fs.createReadStream(map)
+            .pipe(new PNG({
+                filterType: 4
+            }))
+            .on('parsed', function() {
+                
 
-            for (let i = 0; i< hexasphere.tiles.length; i++) {
-                let latLon = hexasphere.tiles[i].getLatLon(radius);
-                let size = box ? computeCoverForTiles_box(this, hexasphere.tiles[i], radius) : computeCoverForTiles_sample(this, hexasphere.tiles[i], radius);
+                for (let i = 0; i< hexasphere.tiles.length; i++) {
+                    let latLon = hexasphere.tiles[i].getLatLon(radius);
+                    let size = box ? computeCoverForTiles_box(this, hexasphere.tiles[i], radius) : computeCoverForTiles_sample(this, hexasphere.tiles[i], radius);
 
-                // threshold < 0 is basically a ignore value
-                if (size > threshold || threshold < 0) {
-                    let tile = {lat: rnd(latLon.lat), lon: rnd(latLon.lon), b: [] };
-                    let scale = size - Math.random() * .25;
+                    // threshold < 0 is basically a ignore value
+                    if (size > threshold || threshold < 0 || (size === 0.0 && ocean)) {
+                        let tile = {lat: rnd(latLon.lat), lon: rnd(latLon.lon), b: [] };
+                        if (size === 0.0 && ocean) {
+                            // Mark these out
+                            tile.a = true;
+                            size = 1.0;
+                        } else if (valueFn != null) {
+                            tile.v = valueFn(hexasphere.tiles[i], radius);
+                            console.log(tile.v);
+                        }
+                        let scale = size - Math.random() * .25;
 
-                    for (let j = 0; j< hexasphere.tiles[i].boundary.length; j++) {
-                        let newPoint = hexasphere.tiles[i].boundary[j].segment(hexasphere.tiles[i].centerPoint, size);
-                        tile.b.push({
-                            x: rnd(newPoint.x),
-                            y: rnd(newPoint.y),
-                            z: rnd(newPoint.z)
-                        });
+                        for (let j = 0; j< hexasphere.tiles[i].boundary.length; j++) {
+                            let newPoint = hexasphere.tiles[i].boundary[j].segment(hexasphere.tiles[i].centerPoint, size);
+                            tile.b.push({
+                                x: rnd(newPoint.x),
+                                y: rnd(newPoint.y),
+                                z: rnd(newPoint.z)
+                            });
+                        }
+
+                        o.tiles.push(tiny ? compress(tile) : tile);
                     }
-
-                    o.tiles.push(tiny ? compress(tile) : tile);
                 }
-            }
-            let json = null;
-            if (tiny) {
-                json = JSON.stringify(o);
-            } else {
-                json = JSON.stringify(o, null, 4);
-            }
-            
-            if (dfile) {
-                const debug = new PNG({ width: this.width, height: this.height, colorType: 2 });
-                debug.data = debugBuffer;
-                debug.pack()
-                    .pipe(fs.createWriteStream(dfile))
-                    .on('finish', function() {
-                        console.log(`debug PNG file ${dfile} written`);
-                    });
-            }
+                let json = null;
+                if (tiny) {
+                    json = JSON.stringify(o);
+                } else {
+                    json = JSON.stringify(o, null, 4);
+                }
 
-            ok(json);
+
+                ok(json);
+            });
         });
     });
-
 }
 
 module.exports = createGrid;
